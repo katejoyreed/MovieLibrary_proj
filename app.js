4278d0a05c794af7e896706a3b532ef11e372889
katejoyreed <katejoyreed@gmail.com>
2021-04-02 11:15:28 -0500

    Found and tried a function to create a table, added function to get data from backend

---
+++ app.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+    var data = [["City 1", "City 2", "City 3"],
+    ["New York", "LA", "Seattle"]]
+    var table = makeTable($(document.body), data);
+    return table;
+});
+
+
+
+function populateTable(){
+    $("#movies").html("")
+    $.get("https://localhost:44347/api/Film/", function(data){
+        console.log(data);
+
+        $.each(data, function(index, el){
+            $("#movies").append(`<div>
+                <div>${index}</div>
+                <div style="color:red">Title: ${el.title}</div>
+                <div>Director: ${el.director}</div>
+                <div>Genre: ${el.genre}</div>
+                <button onClick="editMovie(${el.movieId})">CLick me!</button>
+                </div><br>`)
+        })
+       
+    }).fail(function(err){
+        console.log(err)
+    })
+}
+function makeTable(container, data){
+    var table = $("<table/>").addClass('CSSTableGenerator');
+    $.each(data, function(rowIndex, r) {
+        var row = $("<tr/>");
+        $.each(r, function(colIndex, c){
+            row.append($("<t"+(rowIndex == 0 ?
+                "h" : "d") + "/>").text(c));
+        });
+        table.append(row);
+    });
+    return container.append(table);
+}
