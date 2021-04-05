1b4e872f0097a6bb72b10b704a54c1a49a57f89e
katejoyreed <katejoyreed@gmail.com>
2021-04-05 14:36:50 -0500

    Added form and function to send post request with user input. Upon refreshing, the posted JSON object is collected by Get request function

--- app.js
+++ app.js
@@ -26,14 +26,73 @@ $(document).ready(function(){
                     <div>Director: ${el.director}</div>
                     <div>Genre: ${el.genre}</div>
                     <button onClick="editMovie(${el.movieId})">Click me!</button>
-                    </div><br>`)
-            })
+                    </div><br>`);
+            });
         }
-    })
+    });
 //}
+
 $(document).ready(function(){
+    $.fn.Movie = function(){
+        $("#myform").dform({
+            "html" : [{
+                "name": "Title",
+                "id": "txt-title",
+                "caption": "Movie title",
+                "type": "text",
+                "placeholder": "e.g. Title of the movie"
+            },
+            {"type": "br"}]
+
+            
+        });
+    }
+    $(".call-btn-text").click(function(){
+        $.fn.Movie();
+    });
+    
+});
+
+    $(document).ready(function(){
+        $.fn.Movie2 = function(){
+            $("#myform").dform({
+                "html":[{
+                    "name": "Genre",
+                    "id": "txt-genre",
+                    "caption": "Movie genre",
+                    "type": "text",
+                    "placeholder": "e.g. Thriller"
+                },
+            {"type": "br"}
+        ]
+            });
+        }
+        $(".call-btn-text").click(function(){
+            $.fn.Movie2();
+        });
+    });
+    $(document).ready(function(){
+        $.fn.Movie3 = function(){
+            $("#myform").dform({
+                "html":[{
+                    "name": "Director",
+                    "id": "txt-director",
+                    "caption": "Movie director",
+                    "type": "text",
+                    "placeholder": "e.g. STINKY Zack Snyder"
+                },
+            {"type": "br"}
+        ]
+            });
+        }
+        $(".call-btn-text").click(function(){
+            $.fn.Movie3();
+        });
+    });
+
+$(document).ready(function() {
     var rowIdx = 0;
-    $('#addBtn').on('click',function(){
+    $('#addBtn').on('click',function() {
         $('#tbody').append(`<tr id="R${++rowIdx}"
         <td class="row-index text-center">
         <p>Row ${rowIdx}</p>
@@ -44,23 +103,43 @@ $(document).ready(function(){
         </td>
         <tr>;`);
     });
-    $('#tbody').on('click', 'remove', function(){
-        var movie = $(this).closest('tr').nextAll();
-        movie.each(function(){
+    $('#tbody').on('click', 'remove', function() {
+        var child = $(this).closest('tr').nextAll();
+        child.each(function() {
             var id = $(this).attr('id');
-            var idx = $(this).movies('.row-index').movies('p');
+            var idx = $(this).children('.row-index').children('p');
             var dig = parseInt(id.substring(1));
             idx.html(`Row ${dig - 1}`);
             $(this).attr('id',`R${dig - 1}`);
             $(this).closest('tr').remove();
             rowIdx--;
-        })
-    })
+        });
+    });
 
 })
 
 $("button").click(function(){
-    $.post("https://localhost:44347/api/Film/")
+    $.post("https://localhost:44347/api/Film/"),
+    {
+        
+    }
 })
+
+function processPostForm(){
+    $.ajax({
+        url: "https://localhost:44347/api/Film/",
+        contentType: 'application/json',
+        data: JSON.stringify({"title": $('#title').val(), "genre": $('#genre').val(), "director": $('#director').val()}),
+        headers: {'Access-Control-Allow-Origin': '*'},
+        dataType: 'json',
+        type: 'POST',
+        success: function(data){
+            $('#postResponse').html(data);
+        },
+        
+       
+    });
+    $('#postRequest').submit(processPostForm);
+}
 
 
