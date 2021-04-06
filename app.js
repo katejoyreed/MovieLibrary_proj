1567aa610394450e453ad172cf9d7f7781438214
katejoyreed <katejoyreed@gmail.com>
2021-04-05 20:22:07 -0500

    Added provisional logic for delete function to app.js

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 
-
-
-$(document).ready(function(){
-  
-   
-});
-
-
-
-
 function processPostForm(){
     $.ajax({
         url: "https://localhost:44347/api/Film/",
@@ -24,6 +14,10 @@ function processPostForm(){
        
     });
     $('#postRequest').submit(processPostForm);
+    
+
+
+
 }
 
 function processPutForm(id){
@@ -36,5 +30,69 @@ function processPutForm(id){
         type: 'PUT',
     })
 }
+function setup(){
+    var selectRow = null;
+    var keys = ["title","genre","director"];
 
+    function resetValues(title, genre, director){
+        forEachInput(function(key){
+            $("#"+key).val('');
+        });
+    }
+    function forEachInput(callback){
+        for(var i = 0; i <keys.length; i++){
+            callback(keys[i]);
+        }
+    }
+}
+function createRow(){
+    var row = $(".tr_clone").clone()
+    var newRow = row.appendTo("table").removeClass("tr_clone").fadeIn(1000);
+    forEachInput(function(key){
+        newRow.find("."+key).text($("#"+key).val());
+    });
+    resetValues();
+    selectedRow = null;
+}
+function editRow(row){
+    forEachInput(function(key){
+        $("#" + key).val(row.find("."+key).text());
+    })
+    selectedRow = row;
+}
+$("#display").on("click", function(){
+    if(selectedRow === null){
+        createRow();
+        resetValues();
+    }
+    else {
+        applyValues(selectedRow);
+        resetValues();
+        selectedRow = null;
+    }
+});
+$("#table").delegate("button").on("click", function(e){
+    e.preventDefault();
+    if($(e.target).hasClass('edit')){
+        editRow($(e.target).parent("td").parent("tr"));
+    }
+    else {
+        $(e.target).parent("td").parent("tr").fadeOut(1000).remove();
+    }
+});
+
+function deleteRow(){
+    var id = $('#data[i]');
+$.ajax({
+        
+        url: "https://localhost:44347/api/Film/" + id,
+        contentType: 'application/json',
+        headers: {'Access-Control-Allow-Origin': '*'},
+        dataType: 'json',
+        type: 'DELETE',
+        success: function(){
+            getAllMovies();
+        }
+})
+}
 
